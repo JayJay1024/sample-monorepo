a63410f16ae072e7ac429a94fb3a97bfabb4e496
JayJay1024 <lygm1062@gmail.com>
2023-06-08 18:17:30 +0800

    Three.js

---
+++ packages/three-js/README.md
@@ -0,0 +1,6 @@
+# three-js
+
+### Refer
+
+- https://threejs.org/
+- https://juejin.cn/post/7197324377943081019
--- packages/three-js/src/cube.js
+++ packages/three-js/src/cube.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,7 +13,7 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 3;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -23,4 +24,10 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-animate();
+if (WebGL.isWebGLAvailable()) {
+  // Initiate function or other initializations here
+  animate();
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.body.appendChild(warning);
+}
