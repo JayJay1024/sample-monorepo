c6b330e07216fea223bfedc66702448b30bde064
JayJay1024 <lygm1062@gmail.com>
2023-08-10 16:05:51 +0800

    connect chain

--- packages/ethers-v6/src/index.ts
+++ packages/ethers-v6/src/index.ts
@@ -1,5 +1,18 @@
+import { ethers } from "ethers";
+
 async function main() {
-  console.log("main func");
+  try {
+    // setup
+    const provider = new ethers.JsonRpcProvider("https://ethereum.publicnode.com");
+
+    // action
+    const blockNumber = await provider.getBlockNumber();
+
+    // output
+    console.log("block number:", blockNumber);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 main();
